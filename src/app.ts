3ddbbe129d076fa9cfefec5866430b9d05d61f1f
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-09-17 19:48:29 +0600

    Hardcoded DB is used

--- src/app.ts
+++ src/app.ts
@@ -2,26 +2,62 @@ import fs from "fs";
 import path from "path";
 import express, { Application, Request, Response, NextFunction } from "express";
 import { ApolloServer } from "apollo-server-express";
+import { GraphQLScalarType } from "graphql";
+
+const GraphQLDate = new GraphQLScalarType({
+	name: "GraphQLDate",
+	description: "A Date() type is GraphQL as a scaler",
+	serialize(value) {
+		return value.toISOString();
+	},
+});
 
 // hard coded variables
 let aboutMessage: string = "Issue Tracker API v1.0";
+const issueDB = [
+	{
+		id: 1,
+		status: "New",
+		owner: "Ravan",
+		effort: 5,
+		created: new Date("2019-01-15"),
+		due: undefined,
+		title: "Error in console wehn clicking Add",
+	},
+	{
+		id: 2,
+		status: "Assigned",
+		owner: "Eddie",
+		effort: 14,
+		created: new Date("2019-01-16"),
+		due: new Date("2019-02-01"),
+		title: "Missing bottom border on panel",
+	},
+];
 
 const setAboutMessages = (parent: any, { message }: { message: string }) => {
 	return (aboutMessage = message);
 };
 
-const schemaFile = path.join(__dirname, "../schemas/schema.graphql");
+const issueList = () => {
+	return issueDB;
+};
+
+// loading schema file, typeDefs and resolvers
+const schemaFile = path.join(__dirname, "./schemas/schema.graphql");
 const typeDefs = fs.readFileSync(schemaFile, "utf-8");
 
 // graphQL resolvers
 const resolvers = {
 	Query: {
 		about: () => aboutMessage,
+		issueList,
 	},
 
 	Mutation: {
 		setAboutMessages,
 	},
+	GraphQLDate,
 };
 
 // graphQL server setup
